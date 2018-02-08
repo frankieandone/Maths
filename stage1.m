d7efa8a8cb01f52f755d669c6e0760a37a708672
Frankie <texxeption@protonmail.com>
2018-02-08 00:00:48 +0000

    Added row swapping and comments in stage1.m.

--- stage1.m
+++ stage1.m
@@ -13,9 +13,36 @@ function [U] = stage1(A)
     
     % A is a square matrix then m=n therefore m and n can be used
     % interchangeably.
+    % n represents the outer boundaries (since m=n) of the matrix.
     for i = 1:n-1
+        % A(i:n,i) gets all elements from row i to n in column i.
+        % abs(A(i:n,i)) gets the absolute values for these elements.
+        % max(abs(A(i:n,i))) gets the largest absolute value.
+        % v (~) holds the largest absolute value. It is not used therefore
+        % ~ is used as an anonymous variable; v is needed to make sure that
+        % return of max(abs(A(i:n,i))) is dimensionally compatible.
+        % k represents position of v in relation to row i. e.g.: previous
+        % row to i, next row to i, etc.
+        [~, k] = max(abs(A(i:n,i)));
+        
+        % If k = 1 then it means same row, no point checking the row in
+        % that case. k > 1 prevents useless iteration.
+        if k > 1
+            % Variable temp holds a temporary copy of current row.
+            temp = A(i,:);
+            % Move next row into current row.
+            A(i,:) = A(i+k-1,:);
+            % Copy the temporary row into the next row completing the swap.
+            A(i+k-1,:) = temp;
+        end
+        
         % The pivot is the element in the diagonal line.
         pivot = A(i,i);
+        
+        % Guard against divide by zero errors.
+        if pivot == 0
+            break;
+        end
         
         % If the pivot is not 1 then simplify the row where the
         % pivot is 1. Any changes made is done to the pivot is done
